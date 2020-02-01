1025d769be26ff57bed4e12b4d50762ffaf2e795
a4k9 <vansh.a4k9@gmail.com>
2020-02-02 05:02:42 +0530

    Congtacts Page Error

--- lib/pages/Contacts.dart
+++ lib/pages/Contacts.dart
@@ -2,13 +2,94 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(ContactsPage());
 
+class Contact{
+  const Contact({this.title});
+  final String title;
+}
+
+const List<Contact> contacts = const <Contact>[
+  const Contact(title: 'Hostels'),
+  const Contact(title: 'Helpline'),
+  const Contact(title: 'Administration'),
+  const Contact(title: 'Hostels'),
+  const Contact(title: 'Hostels'),
+  const Contact(title: 'Hostels'),
+  const Contact(title: 'Hostels'),
+  const Contact(title: 'Hostels'),
+  const Contact(title: 'Hostels'),
+  const Contact(title: 'Hostels'),
+];
+
+
 class ContactsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Contacts Page'),
+    return Scaffold(
+      // appBar: AppBar(
+      //   title : Text('Contacts',
+      //   textAlign: TextAlign.center,
+      //   )
+      // ),
+      backgroundColor: Color.fromRGBO(18, 18, 18, 1.0),
+      body : Container(
+        //constraints: BoxConstraints(maxHeight: double.infinity),
+        //height: 1298,
+        child: Column(
+          //mainAxisSize: MainAxisSize.min,
+          children: [
+            Flexible(
+            flex: 1,
+            child: 
+            GridView.count(
+              // primary: false,
+              shrinkWrap: true,
+              //addRepaintBoundaries: true,
+              crossAxisCount: 2,
+              children: List.generate(contacts.length, (index) {
+                return Center(
+                  child: ContactCard(contact: contacts[index]),
+                );
+              }),
+            )),
+            Flexible(
+            flex: 0,
+            child: 
+            Center(
+              child: Text("That's all Folks!")
+            ))
+          ]
+        ),
+      )
+      // bottomNavigationBar: BottomAppBar(
+      //   child: Text('That\u0027s all folks'),
+      //   //clipBehavior: Clip.none,
+      //   color: Color.fromRGBO(18, 18, 18, 1.0),
+      // ),
+    ); 
+  }
+}
+
+class ContactCard extends StatelessWidget{
+  const ContactCard({Key key, this.contact}) : super(key: key);
+  final Contact contact;
+
+  @override
+  Widget build(BuildContext context){
+    //final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      //margin: EdgeInsets.,
+      color: Colors.black26,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0)
       ),
+      child: Center(
+        child: Text(contact.title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15.0
+          )
+        ),
+      )
     );
   }
 }

fc886c50c81328d291f5197b711e3f29792fe523
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-02-02 16:17:34 +0530

    basic filter

--- lib/pages/MapsPage/Maps.dart
+++ lib/pages/MapsPage/Maps.dart
@@ -14,24 +14,34 @@ class MapsPage extends StatefulWidget {
 class _MyAppState extends State<MapsPage> {
   GoogleMapController mapController;
   final LatLng _center = const LatLng(20.354890, 85.815120);
+  static String category = 'all';
+  Set<Marker> _markers = markers[category];
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
     mapController.setMapStyle(null);
-    mapController.setMapStyle('[{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"color":"#c6c6c6"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#ef2110"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"poi.sports_complex","elementType":"geometry.fill","stylers":[{"color":"#7dec5e"}]},{"featureType":"poi.sports_complex","elementType":"labels.text","stylers":[{"color":"#fafafa"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]');
+    mapController.setMapStyle(
+        '[{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"color":"#c6c6c6"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#ef2110"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"poi.sports_complex","elementType":"geometry.fill","stylers":[{"color":"#7dec5e"}]},{"featureType":"poi.sports_complex","elementType":"labels.text","stylers":[{"color":"#fafafa"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]');
+    print(_markers);
     _getPermissions();
+    _currentLocation();
   }
 
-  Future<void> _getPermissions() async{
-    Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.location]);
-    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.location);
-    ServiceStatus serviceStatus = await PermissionHandler().checkServiceStatus(PermissionGroup.location);
-    bool isShown = await PermissionHandler().shouldShowRequestPermissionRationale(PermissionGroup.location);
-    print(isShown);
+  Future<void> _getPermissions() async {
+    Map<PermissionGroup, PermissionStatus> permissions =
+        await PermissionHandler()
+            .requestPermissions([PermissionGroup.location]);
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.location);
+    ServiceStatus serviceStatus =
+        await PermissionHandler().checkServiceStatus(PermissionGroup.location);
+    bool isShown = await PermissionHandler()
+        .shouldShowRequestPermissionRationale(PermissionGroup.location);
   }
-  
+
   void _currentLocation() async {
-    Position currentLocation = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    Position currentLocation = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
 
     mapController.animateCamera(CameraUpdate.newCameraPosition(
       CameraPosition(
@@ -42,12 +52,19 @@ class _MyAppState extends State<MapsPage> {
     ));
   }
 
+  _openSearchFilter(String cat) {
+    setState(() {
+      category = cat;
+      _markers = markers[category];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         onMapCreated: _onMapCreated,
-        markers: markers,
+        markers: _markers,
         initialCameraPosition: CameraPosition(
           target: _center,
           zoom: 18.0,
@@ -62,14 +79,36 @@ class _MyAppState extends State<MapsPage> {
         rotateGesturesEnabled: true,
         tiltGesturesEnabled: true,
         indoorViewEnabled: true,
-        
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           FloatingActionButton(
+            child: Icon(Icons.fastfood),
+            heroTag: hashCode,
+            onPressed: () {
+              _openSearchFilter('foodcourts');
+            },
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.home),
+            heroTag: hashCode,
+            onPressed: () {
+              _openSearchFilter('campuses');
+            },
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          FloatingActionButton(
             child: Icon(Icons.search),
             heroTag: hashCode,
+            onPressed: () {
+              _openSearchFilter('all');
+            },
           ),
           SizedBox(
             height: 16,
--- lib/pages/MapsPage/assets/mapMarkers.dart
+++ lib/pages/MapsPage/assets/mapMarkers.dart
@@ -1,6 +1,7 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-Set<Marker> markers = {
+//Campus
+Set<Marker> campuses = {
   Marker(
     markerId: MarkerId('Campus_15'),
     position: LatLng(20.348698, 85.816095),
@@ -9,13 +10,40 @@ Set<Marker> markers = {
       snippet: 'Computer Department',
     ),
   ),
-
   Marker(
     markerId: MarkerId('Campus_3'),
     position: LatLng(20.353835, 85.816489),
     infoWindow: InfoWindow(
       title: 'Campus 3',
-      snippet: 'Civil Department'
+      snippet: 'Civil Department',
     ),
   ),
+};
+
+//Food Courts
+Set<Marker> foodcourts = {
+  Marker(
+    markerId: MarkerId('KP6_FC'),
+    position: LatLng(20.349253, 85.815905),
+    infoWindow: InfoWindow(
+      title: 'Campus 15 Food Court',
+      snippet: 'Near KP 6',
+    ),
+  ),
+  Marker(
+    markerId: MarkerId('KP7_FC'),
+    position: LatLng(20.350910, 85.815964),
+    infoWindow: InfoWindow(
+      title: 'KP 7 Food Court',
+      snippet: 'Amazon Delivery Stop',
+    ),
+  ),
+};
+
+Set<Marker> all = new Set.from(campuses)..addAll(foodcourts);
+
+Map<String, Set<Marker>> markers = {
+  'all': all,
+  'campuses': campuses,
+  'foodcourts': foodcourts,
 };

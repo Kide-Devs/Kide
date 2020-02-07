dead3474eab663abeb2749ea0ac62796648b5fee
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-02-07 23:38:54 +0530

    App Icon + Home Button

--- lib/widgets/BottomNav.dart
+++ lib/widgets/BottomNav.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kide/assets/Icon/custom_icons_icons.dart';
 import '../providers/router.dart';
 import "package:provider/provider.dart";
 
 class BottomNav extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     final indexState = Provider.of<Router>(context);
@@ -22,46 +22,57 @@ class BottomNav extends StatelessWidget {
       items: [
         BottomNavigationBarItem(
           icon: Icon(Icons.map),
-          title: Text('Maps',
-          style: TextStyle(
-            fontSize: 12.0,
-          ),
+          title: Text(
+            'Maps',
+            style: TextStyle(
+              fontSize: 12.0,
+            ),
           ),
           backgroundColor: Colors.black87,
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.perm_contact_calendar),
-          title: Text('Contacts', 
-          style: TextStyle(
-            fontSize: 12.0,
-          ),
+          title: Text(
+            'Contacts',
+            style: TextStyle(
+              fontSize: 12.0,
+            ),
           ),
           backgroundColor: Colors.black87,
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home', 
-          style: TextStyle(
-            fontSize: 12.0,
+          icon: AnimatedContainer(
+            duration: Duration(milliseconds: 350),
+            child: Image.asset(
+              "./lib/assets/Icon/kide_logoldpi.png",
+              width: _currentIndex == 2 ? 34 : 28,
+              height: _currentIndex == 2 ? 34 : 28,
+            ),
           ),
+          title: Text(
+            'KIDE',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 5.0, letterSpacing: 5),
           ),
           backgroundColor: Colors.black87,
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.event_note),
-          title: Text('Events', 
-          style: TextStyle(
-            fontSize: 12.0,
-          ),
+          title: Text(
+            'Events',
+            style: TextStyle(
+              fontSize: 12.0,
+            ),
           ),
           backgroundColor: Colors.black87,
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.more_horiz),
-          title: Text('More', 
-          style: TextStyle(
-            fontSize: 12.0,
-          ),
+          title: Text(
+            'More',
+            style: TextStyle(
+              fontSize: 12.0,
+            ),
           ),
           backgroundColor: Colors.black87,
         ),

e27e6286502fd7d4ab66de45c9920ab2b8a21c9d
a4k9 <vansh.a4k9@gmail.com>
2020-02-08 01:32:57 +0530

    Contacts Padding + Dynamic Dividers

--- lib/pages/ContactsPage/ContactList.dart
+++ lib/pages/ContactsPage/ContactList.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:kide/models/ContactCategory.dart';
 import 'package:kide/pages/ContactsPage/widgets/ContactCardDiallable.dart';
 import 'package:kide/widgets/HeaderWidget.dart';
-
+import 'package:kide/config/Viewport.dart';
 
 
 class ContactList extends StatelessWidget {
+  // Offset _offset = Offset(0.95, 0.55);
   static const routeName = '/ContactList';
   @override
   Widget build(BuildContext context) {
+    ViewPort().init(context);
     final ContactCategory contactCategory = ModalRoute.of(context).settings.arguments;
     final _contactList = contactCategory.contacts;
     return Scaffold(
@@ -20,39 +22,55 @@ class ContactList extends StatelessWidget {
         ),
       ),
       backgroundColor: Color.fromRGBO(18, 18, 18, 1.0),
-      body : Container(
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  HeaderWidget(contactCategory.name,12),
-                  Divider(
-                    color: Colors.white,
-                    endIndent: 194.0,
-                  ),
-                  // RotationTransition(
-                  //   turns: AlwaysStoppedAnimation(750/360),
-                  //   child: Divider(
-                  //     height: 0.0,
-                  //     color: Colors.white,
-                  //     indent: 163.5,
-                  //     endIndent: 175.0,
-                  //   ),
-                  // ),
-                ],
+      body : Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    HeaderWidget(contactCategory.name,12),
+                    Divider(
+                      color: Colors.white,
+                      endIndent: ViewPort.screenWidth*0.44,
+                    ),
+                    // Transform(
+                    //   transform: Matrix4.identity()
+                    //   ..setEntry(1, 3, 0.001) // perspective
+                    //   ..rotateX(_offset.dy)
+                    //   ..rotateY(_offset.dx),
+                    //   alignment: FractionalOffset(0.57,0.57),
+                    //   child: Divider(
+                    //     height: 0.0,
+                    //     color: Colors.white,
+                    //     indent: ViewPort.screenWidth*0.49,
+                    //     endIndent: ViewPort.screenWidth*0.37,
+                    //   ),
+                    // )
+                    // RotationTransition(
+                    //   turns: AlwaysStoppedAnimation(750/360),
+                    //   child: Divider(
+                    //     height: 0.0,
+                    //     color: Colors.white,
+                    //     indent: 163.5,
+                    //     endIndent: 175.0,
+                    //   ),
+                    // ),
+                  ],
+                ),
               ),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  for(int i=0;i<_contactList.length;i++)
-                    ContactCardDiallable(_contactList[i].name, _contactList[i].number, _contactList[i].designation),
-                ],
-              ),
-            )
-          ]
-        )
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    for(int i=0;i<_contactList.length;i++)
+                      ContactCardDiallable(_contactList[i].name, _contactList[i].number, _contactList[i].designation),
+                  ],
+                ),
+              )
+            ]
+          )
+        ),
       )
     ); 
   }

02b061e2b33e82695a40bea17da3ad7eae410d87
a4k9 <vansh.a4k9@gmail.com>
2020-02-14 01:46:42 +0530

    Redesigned MoreHome (Page)

---
+++ lib/models/Official.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+class Official {
+  final String name;
+  final String designation;
+  Official({
+    @required this.name,
+    @required this.designation,
+  });
+}

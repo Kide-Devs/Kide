25b918be166b1edc8b54ca5b25f99f2364cad481
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-02-20 14:59:37 +0530

    Rose Garden picture updated

--- lib/pages/ForYou/ForYou.dart
+++ lib/pages/ForYou/ForYou.dart
@@ -1,23 +1,17 @@
 import 'package:Kide/pages/ForYou/widgets/MustSeeWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:Kide/config/Viewport.dart';
-import 'package:Kide/providers/getEvents.dart';
-import 'package:Kide/util/constants.dart';
-import 'package:provider/provider.dart';
-import 'package:Kide/widgets/HeaderWidget.dart';
+// import 'package:Kide/providers/getEvents.dart';
+// import 'package:Kide/util/constants.dart';
+// import 'package:provider/provider.dart';
+// import 'package:Kide/widgets/HeaderWidget.dart';
 
 void main() => runApp(ForYouPage());
 
 class ForYouPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Events Listener
-    final _getEvents = Provider.of<GetEvents>(context);
-    // for Events
-    if (_getEvents.eventList.length == 0) _getEvents.setEvents();
-
     ViewPort().init(context);
-    print("sub events ${_getEvents.eventCategories}");
     return Scaffold(
       backgroundColor: Color.fromRGBO(18, 18, 18, 1.0),
       body: ListView(
@@ -78,7 +72,7 @@ class ForYouPage extends StatelessWidget {
                           ),
                           Spacer(),
                           MustSeeWidget(
-                            src: "https://secure.s.forbestravelguide.com/img/properties/hotel-new-otani-tokyo-executive-house-zen/hotel-new-otani-tokyo-executive-house-zen-rose-garden-gazebo.jpg",
+                            src: "https://i.ytimg.com/vi/gVlEh_VfFvo/maxresdefault.jpg",
                             title: "Rose Garden",
                             marker: "rose_garden"
                           )
@@ -96,48 +90,45 @@ class ForYouPage extends StatelessWidget {
     );
   }
 
-  Card _buildEventCard(
-      BuildContext context, GetEvents _getEvents, int position) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-      child: InkWell(
-        splashColor: Colors.blue.withAlpha(30),
-        onTap: () {
-          // Navigator.of(context).pushNamed(SubEvents.routeName,
-          //     arguments: _getEvents.eventCategories[position]);
-          print('Card tapped.');
-        },
-        child: Stack(children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(8.0),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(
-                  EVENTS_PAGE_EVENT_JPG,
-                ),
-              ),
-            ),
-            height: 120.0,
-          ),
-          Container(
-              height: 120.0,
-              decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.64),
-                  borderRadius: BorderRadius.circular(8.0)),
-              child: Center(
-                child: Text(
-                  _getEvents.eventCategories[position].name.toUpperCase(),
-                  style: TextStyle(
-                      color: Colors.white,
-                      letterSpacing: ViewPort.screenWidth * 0.05,
-                      fontWeight: FontWeight.w600),
-                ),
-              )),
-        ]),
-      ),
-      margin: const EdgeInsets.fromLTRB(0.0, 14.0, 0.0, 14.0),
-    );
-  }
+//   Card _buildEventCard(
+//       BuildContext context, GetEvents _getEvents, int position) {
+//     return Card(
+//       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+//       child: InkWell(
+//         splashColor: Colors.blue.withAlpha(30),
+//         onTap: () {
+//         },
+//         child: Stack(children: <Widget>[
+//           Container(
+//             decoration: BoxDecoration(
+//               color: Colors.transparent,
+//               borderRadius: BorderRadius.circular(8.0),
+//               image: DecorationImage(
+//                 fit: BoxFit.cover,
+//                 image: AssetImage(
+//                   EVENTS_PAGE_EVENT_JPG,
+//                 ),
+//               ),
+//             ),
+//             height: 120.0,
+//           ),
+//           Container(
+//               height: 120.0,
+//               decoration: BoxDecoration(
+//                   color: Colors.black.withOpacity(0.64),
+//                   borderRadius: BorderRadius.circular(8.0)),
+//               child: Center(
+//                 child: Text(
+//                   _getEvents.eventCategories[position].name.toUpperCase(),
+//                   style: TextStyle(
+//                       color: Colors.white,
+//                       letterSpacing: ViewPort.screenWidth * 0.05,
+//                       fontWeight: FontWeight.w600),
+//                 ),
+//               )),
+//         ]),
+//       ),
+//       margin: const EdgeInsets.fromLTRB(0.0, 14.0, 0.0, 14.0),
+//     );
+//   }
 }
